28e5ad95981060fcd5f7b451ca5c22ab54df442d
dominik-air <domzurek@student.agh.edu.pl>
2022-07-19 22:00:27 +0200

    working websocket example

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import networkx as nx
+import asyncio
+from fastapi import FastAPI, WebSocket
+import sim
 
-if __name__ == "__main__":
-    G = nx.Graph()
-    print(G)
+app = FastAPI()
+robot = sim.Robot(x=10, y=10, width=10, length=10)
+points = [(20, 40), (80, 80)]
+moves = iter(sim.create_instruction_set(points, step_size=10))
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        await asyncio.sleep(1)
+        robot.move(*next(moves))
+        await websocket.send_text(f"(x:{robot.x}; y:{robot.y})")
